b892b23b92b68bed94cb75463652c589f50e6d1a
Hernan Aracena <h@oja.la>
2013-08-12 18:16:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nirvana::Application.config.secret_token = 'e40087679ededfda31ac1614c53c36dc9b944a27dbdf2fad90af324dc39b27f660843d8805726847c0959b87a4b8252909eb721b14d1103e395de214a96e6744'
